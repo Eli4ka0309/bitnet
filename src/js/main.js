48761e9c3a37366ea14a6c838e5664a2263efb9d
Eli4ka0309 <100150012+Eli4ka0309@users.noreply.github.com>
2023-02-14 17:37:10 +0200

    add carusel

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.slides_inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        adaptiveHeight: true,
+        touchThreshold: 10,
+        centerMode:true,
+        variableWidth: true
+
+
+
+    });
+
+});
